931fb42d53da0c6aa5288a0991194e0b666fc7f0
Lisandro Bertoli <lisandro.bertoli@gmail.com>
2022-01-25 17:39:35 +0100

    red

--- src/test/java/kata/BowlingGameShould.java
+++ src/test/java/kata/BowlingGameShould.java
@@ -6,7 +6,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BowlingGameShould {
     @Test
-    void  return_0_when_miss()
+    void return_0_when_miss()
     {
         var bowlingGame = new BowlingGame();
         var result = bowlingGame.score("-");
@@ -15,7 +15,7 @@ public class BowlingGameShould {
     }
 
     @Test
-    void  return_1_when_only_one_pin_thrown()
+    void return_1_when_only_one_pin_thrown()
     {
         var bowlingGame = new BowlingGame();
         var result = bowlingGame.score("1");
@@ -24,7 +24,7 @@ public class BowlingGameShould {
     }
 
     @Test
-    void  return_2_when_only_two_pin_thrown()
+    void return_2_when_only_two_pin_thrown()
     {
         var bowlingGame = new BowlingGame();
         var result = bowlingGame.score("2");
@@ -33,7 +33,7 @@ public class BowlingGameShould {
     }
 
     @Test
-    void  return_2_when_only_two_pin_thrown_with_two_tries()
+    void return_2_when_only_two_pin_thrown_with_two_tries()
     {
         var bowlingGame = new BowlingGame();
         var result = bowlingGame.score("2-");
@@ -42,7 +42,7 @@ public class BowlingGameShould {
     }
 
     @Test
-    void  return_3_when_only_three_pin_thrown_with_two_tries()
+    void return_3_when_only_three_pin_thrown_with_two_tries()
     {
         var bowlingGame = new BowlingGame();
         var result = bowlingGame.score("21");
@@ -51,7 +51,7 @@ public class BowlingGameShould {
     }
 
     @Test
-    void  return_90_for_the_given_line()
+    void return_90_for_the_given_line()
     {
         var bowlingGame = new BowlingGame();
         var result = bowlingGame.score("9-|9-|9-|9-|9-|9-|9-|9-|9-|9-||");
@@ -60,11 +60,30 @@ public class BowlingGameShould {
     }
 
     @Test
-    void  return_100_for_the_given_line()
+    void return_100_for_the_given_line()
     {
         var bowlingGame = new BowlingGame();
         var result = bowlingGame.score("9/|9-|9-|9-|9-|9-|9-|9-|9-|9-||");
 
         assertEquals(100, result);
+    }
+
+    @Test
+    void return_110_for_the_given_line()
+    {
+        var bowlingGame = new BowlingGame();
+        var result = bowlingGame.score("9/|9-|9/|9-|9-|9-|9-|9-|9-|9-||");
+
+        assertEquals(110, result);
+    }
+
+
+    @Test
+    void return_105_for_the_given_line()
+    {
+        var bowlingGame = new BowlingGame();
+        var result = bowlingGame.score("9/|9-|9-|9-|9-|9-|9-|9-|9-|9/||4");
+
+        assertEquals(105, result);
     }
 }
