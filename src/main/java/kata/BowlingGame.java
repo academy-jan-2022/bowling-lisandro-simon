96b6123f055142e469f43605ab85b13e8478e948
Lisandro Bertoli <lisandro.bertoli@gmail.com>
2022-01-25 16:29:44 +0100

    refactor

--- src/main/java/kata/BowlingGame.java
+++ src/main/java/kata/BowlingGame.java
@@ -1,9 +1,9 @@
 package kata;
 
 public class BowlingGame {
-    public int score(String s) {
-        if (s == "1") return 1;
-        if(s == "2") return 2;
+    public int score(String line) {
+        if (line == "1") return 1;
+        if(line == "2") return 2;
 
         return 0;
     }
