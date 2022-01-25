f9f954e894e7e8fef31a69c12554ca95aadf98bc
Simon <simonbesteiro@gmail.com>
2022-01-25 17:51:34 +0100

    green

--- src/main/java/kata/BowlingGame.java
+++ src/main/java/kata/BowlingGame.java
@@ -5,15 +5,19 @@ public class BowlingGame {
         int finalScore = 0;
         int bonusScore = 0;
         boolean isSpare = false;
+        int pipesCounter = 0;
 
         for(var i = 0; i < line.length(); i++){
             var currentChar = line.charAt(i);
             if(!isASimbol(currentChar)){
                 finalScore += Character.getNumericValue(currentChar);
-                bonusScore = isSpare ? bonusScore + Character.getNumericValue(currentChar) : bonusScore;
+                if (pipesCounter< 2){
+                    bonusScore = isSpare ? bonusScore + Character.getNumericValue(currentChar) : bonusScore;
+                    pipesCounter = 0;
+                }
                 isSpare = false;
             }
-
+            if (currentChar == '|') pipesCounter ++;
             if(currentChar == '/'){
                 finalScore += pinsDroppedInSpare(line, i);
                 isSpare = true;
