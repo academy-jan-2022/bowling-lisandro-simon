01729f34fc6070cc61cd59bc4e75371fa9637ee0
Lisandro Bertoli <lisandro.bertoli@gmail.com>
2022-01-25 16:49:44 +0100

    green

--- src/main/java/kata/BowlingGame.java
+++ src/main/java/kata/BowlingGame.java
@@ -2,9 +2,12 @@ package kata;
 
 public class BowlingGame {
     public int score(String line) {
+        int finalScore = 0;
+
         for(var i = 0; i < line.length(); i++){
-            if(line.charAt(i) != '-') return Character.getNumericValue(line.charAt(i));
+            if(line.charAt(i) != '-') finalScore += Character.getNumericValue(line.charAt(i));
         }
-        return 0;
+
+        return finalScore;
     }
 }
