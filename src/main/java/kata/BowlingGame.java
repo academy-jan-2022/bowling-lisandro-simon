6f7e5375017103993586a73d95cc806eafe88bf7
Simon <simonbesteiro@gmail.com>
2022-01-25 16:45:09 +0100

    green

--- src/main/java/kata/BowlingGame.java
+++ src/main/java/kata/BowlingGame.java
@@ -2,10 +2,9 @@ package kata;
 
 public class BowlingGame {
     public int score(String line) {
-        if(line != "-"){
-            return Integer.parseInt(line);
+        for(var i = 0; i < line.length(); i++){
+            if(line.charAt(i) != '-') return Character.getNumericValue(line.charAt(i));
         }
-
         return 0;
     }
 }
