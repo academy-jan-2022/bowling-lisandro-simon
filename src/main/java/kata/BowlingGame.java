f7d5c4122e4fad165bbc9543402641d71367ea32
Simon <simonbesteiro@gmail.com>
2022-01-25 17:03:04 +0100

    refactor

--- src/main/java/kata/BowlingGame.java
+++ src/main/java/kata/BowlingGame.java
@@ -5,9 +5,15 @@ public class BowlingGame {
         int finalScore = 0;
 
         for(var i = 0; i < line.length(); i++){
-            if(line.charAt(i) != '-' && line.charAt(i) != '|') finalScore += Character.getNumericValue(line.charAt(i));
+            if(!isASimbol(line.charAt(i)))
+                finalScore += Character.getNumericValue(line.charAt(i));
         }
 
         return finalScore;
+    }
+
+    private boolean isASimbol(char symbol) {
+        return symbol == '-'
+            || symbol == '|';
     }
 }
